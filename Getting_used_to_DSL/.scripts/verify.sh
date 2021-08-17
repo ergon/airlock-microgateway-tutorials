5f9e912c561f5965cd29bb01e76f2aa9ac28ba08
Roland Weber <roland.weber@ergon.ch>
2021-08-17 13:56:38 +0200

    verification for dsl lab

--- Getting_used_to_DSL/.scripts/verify.sh
+++ Getting_used_to_DSL/.scripts/verify.sh
@@ -1,2 +1,17 @@
 # lab specific additional verifications
-echo "testst"
+minikube_ip=$(minikube ip)
+STATUSCODE=$(curl -k -s -o /dev/null -w "%{http_code}" https://$minikube_ip/)
+if [ "${STATUSCODE}" = "503" ]
+then
+    echo "OK: Microgateway returned status code 503"
+else
+    echo "NOK: Microgateway did not return status code 503"
+fi
+
+REQUEST_COUNT=$(kubectl logs -l app=microgateway -c microgateway --tail=-1 | grep -c "WR-SG-SUMMARY")
+if [ "${REQUEST_COUNT}" -ge "1" ]
+then
+    echo "OK: Microgateway served at least 1 request: ${REQUEST_COUNT}"
+else
+    echo "NOK: Microgateway did not serve request: ${REQUEST_COUNT}"
+fi
