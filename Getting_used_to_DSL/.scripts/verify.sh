db3e62b1c6b1177b53f554067626b4f4dd1a13d4
Roland Weber <roland.weber@ergon.ch>
2021-08-17 14:00:25 +0200

    return exit code from verification script

--- Getting_used_to_DSL/.scripts/verify.sh
+++ Getting_used_to_DSL/.scripts/verify.sh
@@ -1,4 +1,5 @@
 # lab specific additional verifications
+EXIT_CODE=0
 minikube_ip=$(minikube ip)
 STATUSCODE=$(curl -k -s -o /dev/null -w "%{http_code}" https://$minikube_ip/)
 if [ "${STATUSCODE}" = "503" ]
@@ -6,6 +7,7 @@ then
     echo "OK: Microgateway returned status code 503"
 else
     echo "NOK: Microgateway did not return status code 503"
+    EXIT_CODE=1
 fi
 
 REQUEST_COUNT=$(kubectl logs -l app=microgateway -c microgateway --tail=-1 | grep -c "WR-SG-SUMMARY")
@@ -14,4 +16,7 @@ then
     echo "OK: Microgateway served at least 1 request: ${REQUEST_COUNT}"
 else
     echo "NOK: Microgateway did not serve request: ${REQUEST_COUNT}"
+    EXIT_CODE=1
 fi
+
+exit $EXIT_CODE
